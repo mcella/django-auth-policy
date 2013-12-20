09311a99d37c8623a644acb30daf8523a5e7a196
Rudolph Froger <rudolphfroger@estrate.nl>
2013-12-20 09:04:55 +0100

    Fix translatability of validation messages when defined in custom settings

--- django_auth_policy/validators.py
+++ django_auth_policy/validators.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
 
 from django_auth_policy import settings as dap_settings
 
@@ -8,7 +9,7 @@ def password_min_length(value):
         return
 
     if len(value) < dap_settings.PASSWORD_MIN_LENGTH:
-        msg = dap_settings.PASSWORD_MIN_LENGTH_TEXT.format(
+        msg = _(dap_settings.PASSWORD_MIN_LENGTH_TEXT).format(
             length=dap_settings.PASSWORD_MIN_LENGTH)
         raise ValidationError(msg, code='password_min_length')
 
@@ -20,6 +21,6 @@ def password_complexity(value):
     pw_set = set(value)
     for rule in dap_settings.PASSWORD_COMPLEXITY:
         if not pw_set.intersection(rule['chars']):
-            msg = dap_settings.PASSWORD_COMPLEXITY_TEXT.format(
-                rule_text=rule['text'])
+            msg = _(dap_settings.PASSWORD_COMPLEXITY_TEXT).format(
+                rule_text=_(rule['text']))
             raise ValidationError(msg, 'password_complexity')
