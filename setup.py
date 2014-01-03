1bf649207f850b21e99eb6a9479ecdb1cb03a93d
Rudolph Froger <rudolphfroger@estrate.nl>
2014-01-03 14:26:28 +0100

    Update version to 0.7.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='django-auth-policy',
-    version='0.7',
+    version='0.7.1',
     description='Enforces a couple of common authentication policies for the '
                 'Django web framework.',
     author='Fox-IT B.V.',
