bbdf6b4be41935de621eaf6d4f65ee78aaab7e22
Rudolph Froger <rudolphfroger@estrate.nl>
2014-11-04 16:06:42 +0100

    Move checks from management command into Django checks framework

--- django_auth_policy/__init__.py
+++ django_auth_policy/__init__.py
@@ -1,3 +1,5 @@
+default_app_config = 'django_auth_policy.apps.DjangoAuthPolicyConfig'
+
 class BasePolicy(object):
     def __init__(self, **kwargs):
         """ Policy configuration happens at init
