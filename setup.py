7eb2a7e95791d5c2a74b93ecf31cb5f580f1b821
Rudolph Froger <rudolphfroger@estrate.nl>
2014-11-05 15:40:00 +0100

    Up the version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='django-auth-policy',
-    version='0.9.6',
+    version='0.9.7',
     zip_safe=False,
     description='Enforces a couple of common authentication policies for the '
                 'Django web framework.',
