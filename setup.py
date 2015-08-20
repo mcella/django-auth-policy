7b6b0c6ca9922b949b51d755ea7cc2f4a41c9bf3
Ronald Evers <ronaldevers@dreamsolution.nl>
2015-08-20 17:24:17 +0200

    bump to 0.9.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-auth-policy',
-    version='0.9.11.dev',
+    version='0.9.11',
     zip_safe=False,
     description='Enforces a couple of common authentication policies for the '
                 'Django web framework.',
